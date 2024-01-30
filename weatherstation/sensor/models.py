4edd1f22dd90545b6b1ce736504deef6249c4890
AlisaLC <ali_sal1381@yahoo.com>
2024-01-30 23:20:08 +0330

    added str functions

--- weatherstation/sensor/models.py
+++ weatherstation/sensor/models.py
@@ -5,23 +5,41 @@ class Temperature(models.Model):
     value = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.value}°C"
+
 class Humidity(models.Model):
     value = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.value}%"
 
 class AudioNoise(models.Model):
     value = models.BooleanField() # True = noise, False = no noise
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{'Noisy' if self.value else 'Quiet'}"
+
 class Light(models.Model):
     value = models.BooleanField() # True = light, False = no light
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{'Light' if self.value else 'Dark'}"
 
 class AirPollution(models.Model):
     value = models.BooleanField() # True = polluted, False = clean
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{'Polluted' if self.value else 'Clean'}"
+
 class Location(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.latitude}, {self.longitude}"
