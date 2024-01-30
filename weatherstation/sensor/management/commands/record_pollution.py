f8c2bf602d5fa20cfd5c8f9b3cc60d3404a9f2d3
AlisaLC <ali_sal1381@yahoo.com>
2024-01-31 00:15:18 +0330

    changed distance measure

--- weatherstation/sensor/management/commands/record_pollution.py
+++ weatherstation/sensor/management/commands/record_pollution.py
@@ -14,20 +14,15 @@ import requests
 logger = logging.getLogger(__name__)
 
 def KL_divergence(mean1, std1, mean2, std2):
-    if std1 == 0 or std2 == 0:
-        return 0
     return np.log(std2 / std1) + (std1 ** 2 + (mean1 - mean2) ** 2) / (2 * std2 ** 2) - 0.5
 
+def JS_divergence(mean1, std1, mean2, std2):
+    return 0.5 * (KL_divergence(mean1, std1, mean2, std2) + KL_divergence(mean2, std2, mean1, std1))
+
 def air_status(img):
-    KL_red_clear = KL_divergence(190, 30, img[:, :, 0].mean(), img[:, :, 0].std())
-    KL_green_clear = KL_divergence(206, 22, img[:, :, 1].mean(), img[:, :, 1].std())
-    KL_blue_clear = KL_divergence(214, 17, img[:, :, 2].mean(), img[:, :, 2].std())
-    KL_red_polluted = KL_divergence(173, 14, img[:, :, 0].mean(), img[:, :, 0].std())
-    KL_green_polluted = KL_divergence(161, 14, img[:, :, 1].mean(), img[:, :, 1].std())
-    KL_blue_polluted = KL_divergence(153, 13, img[:, :, 2].mean(), img[:, :, 2].std())
-    KL_mean_clear = (KL_red_clear + KL_green_clear + KL_blue_clear) / 3
-    KL_mean_polluted = (KL_red_polluted + KL_green_polluted + KL_blue_polluted) / 3
-    if KL_mean_clear > KL_mean_polluted:
+    KL_blue_clear = JS_divergence(200, 17, img[:, :, 2].mean(), img[:, :, 2].std())
+    KL_blue_polluted = JS_divergence(153, 13, img[:, :, 2].mean(), img[:, :, 2].std())
+    if KL_blue_clear > KL_blue_polluted:
         return True
     else:
         return False
