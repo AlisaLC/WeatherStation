669b1284e14d9e3266aac3acd60c7dc518863915
AlisaLC <ali_sal1381@yahoo.com>
2024-01-31 03:00:58 +0330

    added email

--- weatherstation/monitoring/management/commands/run_bot.py
+++ weatherstation/monitoring/management/commands/run_bot.py
@@ -12,8 +12,10 @@ import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 
 from django.utils import timezone
+from django.db import models
 
 from sensor.models import Temperature, Humidity, AudioNoise, Light, AirPollution, Location
+from notification.models import Email, EmailAttachment
 
 class Command(BaseCommand):
     help = "Run the bot"
@@ -92,7 +94,7 @@ Latest location: {latest_location.latitude:.5f}, {latest_location.longitude:.5f}
             bot.send_photo(message.chat.id, open(data.image.path, "rb"), caption=f"Air pollution: {'Yes' if data.value else 'No'}")
         
         @bot.message_handler(commands=["weather"])
-        def weather(message):
+        def weather(message, send_plot=True):
             location = Location.objects.latest("timestamp")
             r = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={location.latitude}&longitude={location.longitude}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,soil_temperature_0cm,soil_moisture_0_to_1cm&timezone=GMT&past_days=7&forecast_days=1')
             data = r.json()
@@ -109,6 +111,34 @@ Latest location: {latest_location.latitude:.5f}, {latest_location.longitude:.5f}
             fig.add_scatter(x=timestamps, y=soil_temperature, mode="lines", name="Soil temperature")
             fig.add_scatter(x=timestamps, y=soil_moisture, mode="lines", name="Soil moisture")
             fig.write_image("weather.png")
-            bot.send_photo(message.chat.id, open("weather.png", "rb"))
+            if send_plot:
+                bot.send_photo(message.chat.id, open("weather.png", "rb"))
+        
+        @bot.message_handler(commands=["email"])
+        def email(message):
+            latest_temperature = Temperature.objects.latest("timestamp")
+            mean_temperature = Temperature.objects.filter(timestamp__gte=timezone.now()-timezone.timedelta(days=7)).aggregate(models.Avg("value"))["value__avg"]
+            latest_humidity = Humidity.objects.latest("timestamp")
+            mean_humidity = Humidity.objects.filter(timestamp__gte=timezone.now()-timezone.timedelta(days=7)).aggregate(models.Avg("value"))["value__avg"]
+            latest_audio_noise = AudioNoise.objects.latest("timestamp")
+            latest_light = Light.objects.latest("timestamp")
+            latest_air_pollution = AirPollution.objects.latest("timestamp")
+            latest_location = Location.objects.latest("timestamp")
+            email = Email(
+                subject="Weather data",
+                message="Weather data",
+                recipient=settings.EMAIL_RECIPIENT,
+                body=f"""Latest temperature: {latest_temperature.value:.2f}°C
+Last Week Mean temperature: {mean_temperature:.2f}°C
+Latest humidity: {latest_humidity.value:.2f}%
+Last Week Mean humidity: {mean_humidity:.2f}%
+Latest audio noise: {'Yes' if latest_audio_noise.value else 'No'}
+Latest light: {'Yes' if latest_light.value else 'No'}
+Latest air pollution: {'Yes' if latest_air_pollution.value else 'No'}
+Latest location: {latest_location.latitude:.5f}, {latest_location.longitude:.5f}""")
+            email.save()
+            weather(message, send_plot=False)
+            EmailAttachment(email=email, file="weather.png").save()
+            email.send()
 
         bot.polling()
